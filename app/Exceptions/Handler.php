39c8c8db1a390ab6990effd482911a4020de7b1f
kdo70 <d.krivosheev70@gmail.com>
2021-12-01 00:17:27 +0700

    fix exception redirect

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,21 +6,34 @@ use App\View\Components\InformationModal;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+/**
+ *
+ */
 class Handler extends ExceptionHandler
 {
+    /**
+     * @var string[]
+     */
     protected $dontReport = [];
 
+    /**
+     * @var string[]
+     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
+    /**
+     *
+     */
     public function register()
     {
         $this->renderable(function (Exception $exception, $request) {
             $messages = static::getMessages($exception);
-            $status = $this->getStatus($exception);
+            $status = static::getStatus($exception);
+            $redirect = static::getRedirectTo($exception);
 
             $view = app(InformationModal::class, [
                 'messages' => $messages
@@ -34,12 +47,18 @@ class Handler extends ExceptionHandler
 
             if ($request->ajax()) {
                 return response()->json($data, $status);
+            } elseif ($redirect) {
+                return response()->redirectTo($redirect);
             }
 
             return response()->view('errors.show', $data, $status);
         });
     }
 
+    /**
+     * @param $exception
+     * @return mixed
+     */
     protected function getMessages($exception)
     {
         if (method_exists($exception, 'errors')) {
@@ -48,6 +67,10 @@ class Handler extends ExceptionHandler
         return $exception->getMessage();
     }
 
+    /**
+     * @param $exception
+     * @return int
+     */
     protected function getStatus($exception): int
     {
         if (method_exists($exception, 'getStatusCode')) {
@@ -56,5 +79,17 @@ class Handler extends ExceptionHandler
             return $exception->status;
         }
         return 500;
+    }
+
+    /**
+     * @param $exception
+     * @return false|string
+     */
+    public function getRedirectTo($exception)
+    {
+        if (!method_exists($exception, 'redirectTo')) {
+            return false;
+        }
+        return $exception->redirectTo();
     }
 }
